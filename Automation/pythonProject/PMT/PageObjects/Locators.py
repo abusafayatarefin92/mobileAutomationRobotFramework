b082ead943459ee51265d535839c8bf8b7fbb6fd
abusafayatarefin92 <abusafayatarefin92@gmail.com>
2024-06-12 12:17:19 +0600

    Automation add

---
+++ Automation/pythonProject/PMT/PageObjects/Locators.py
@@ -0,0 +1,7 @@
+from collections.abc import Mapping
+#Login page elements
+txt_loginUsername = 'id:Email'
+txt_loginPassword = 'id:Password'
+
+btn_LoginScreen = 'xpath://*[@id="LoginDiv"]/div/div/a'
+btn_Login = 'xpath:/html/body/div/div[2]/div/form/div[3]/button'
